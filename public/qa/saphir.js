476658c0d7ec98696d076df1fdc4170cd8829bcf
IrisBorck <iris_borck@mac.com>
2026-09-25 23:48:02 +0200

    Saphir: Buchungsformular auch bei 320 Pixeln vollständig einpassen

--- public/qa/saphir.js
+++ public/qa/saphir.js
@@ -1,3 +1,5 @@
 const width = document.querySelector('#qa-width');
 const frame = document.querySelector('#qa-frame');
 width.addEventListener('change', () => { frame.style.width = `${Number(width.value)}px`; });
+
+frame.src = `../apartments/saphir/?qa=${Date.now()}`;
