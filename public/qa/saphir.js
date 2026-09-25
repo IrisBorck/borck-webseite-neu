d7d2f8d117095d845d83a3228bd41cf29b9909ce
IrisBorck <iris_borck@mac.com>
2026-09-25 23:40:52 +0200

    Saphir: Browserbefunde korrigieren und responsive Prüfansicht ergänzen

---
+++ public/qa/saphir.js
@@ -0,0 +1,3 @@
+const width = document.querySelector('#qa-width');
+const frame = document.querySelector('#qa-frame');
+width.addEventListener('change', () => { frame.style.width = `${Number(width.value)}px`; });
